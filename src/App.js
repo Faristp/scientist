0b36f8964b35567b1c22269e4eaf97dedc043048
Faristp <farismuhammed101@gmail.com>
2025-05-06 13:27:04 +0530

    khabibs todo's

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-import Profile from "./Profile";
-import './App.css';
-
-export default function Gallery() {
-  return (  
-    <section className="allery">
-      <h1>Gallery</h1>
-   
-      <Profile/>
-      <Profile/> 
-      <Profile/>
-      <Profile/>
-  
-      
-      
-    </section>
+export default function TodoList() {
+  return (
+    <>
+      <h1>khabib's ToDo'S</h1>
+      <img src="khabib.jpg" alt="khabib" class="photo" />
+      <ul>
+        <li>Train</li>
+        <li>Eat</li>
+        <li>Compete</li>
+        <li>Sleep</li>
+      </ul>
+    </>
   );
 }
