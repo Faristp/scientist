6b4fe67f9344cf04e4c9233a2316f4783a2bee80
Faristp <farismuhammed101@gmail.com>
2025-05-06 16:28:43 +0530

    list rendering

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-function Item({ name, isPacked }) {
-  return (
-    <li>
-      {name}
-      {isPacked && " ✔"}
-    </li>
-  );
-}
+import { people } from "./data.js";
+import getImageUrl from "./utils.js";
 
-export default function PackingList() {
+export default function List() {
+  const listItems = people.map((person) => (
+    <li key={person.id} style={{ display: "flex", marginBottom: "10px" }}>
+      <img
+        src={getImageUrl(person)}
+        alt={person.name}
+        style={{ borderRadius: "50%", marginRight: "100px" }}
+      />
+      <p style={{ maxWidth: "400px" }}>
+        <b>{person.name}:</b>
+        {" " + person.profession} known for {person.accomplishment}
+        <br />
+      </p>
+    </li>
+  ));
+
   return (
-    <section>
-      <h1>My Packing List</h1>
-      <ul>
-        <Item name="Car Key" isPacked={true} />
-        <Item name="Sunglasses" isPacked={false} />
-        <Item name="Food" isPacked={true} />
-      </ul>
-    </section>
+    <article>
+      <h1 style={{ marginLeft: "40px" }}> Scientist</h1>
+      <ul>{listItems}</ul>
+    </article>
   );
 }
