b9425166b40cf6be223d204f1d54bb6a8501b2f6
Faristp <farismuhammed101@gmail.com>
2025-05-07 13:57:44 +0530

    Updatin objects in state

--- src/App.js
+++ src/App.js
@@ -1,29 +1,74 @@
 import { useState } from "react";
 
-export default function App() {
-  const [score, setScore] = useState(0);
+export default function Form() {
+  const [person, setPerson] = useState({
+    name: "Niki de Saint Phalle",
+    artwork: {
+      title: "Blue Nana",
+      city: "Hamburg",
+      image: "https://i.imgur.com/Sd1AgUOm.jpg",
+    },
+  });
 
-  function increment() {
-    setScore((score) => score + 1);
+  function handleChangeName(e) {
+    setPerson({
+      ...person,
+      name: e.target.value,
+    });
   }
-  function decrement() {
-    setScore((score) => score - 1);
+
+  function handleChangeTitle(e) {
+    setPerson({
+      ...person,
+      artwork: {
+        ...person.artwork,
+        title: e.target.value,
+      },
+    });
+  }
+
+  function handleChangeCity(e) {
+    setPerson({
+      ...person,
+      artwork: {
+        ...person.artwork,
+        city: e.target.value,
+      },
+    });
+  }
+  function handleChangeImage(e) {
+    setPerson({
+      ...person,
+      artwork: {
+        ...person.artwork,
+        image: e.target.value,
+      },
+    });
   }
 
   return (
     <>
-      <button onClick={increment}>+1</button>
-      <button
-        onClick={() => {
-          increment();
-          increment();
-          increment();
-        }}
-      >
-        +3
-      </button>
+      <label>
+        Name:
+        <input value={person.name} onChange={handleChangeName} />
+      </label>
 
-      <h1>{score}</h1>
+      <lable>
+        Artwork:
+        <input value={person.artwork.title} onChange={handleChangeTitle} />
+      </lable>
+      <label>
+        City:
+        <input value={person.artwork.city} onChange={handleChangeCity} />
+      </label>
+      <label>
+        Image:
+        <input value={person.artwork.image} onChange={handleChangeImage} />
+      </label>
+      <h1>{person.name}</h1>
+      <h2>{person.artwork.title}</h2>
+      <h3>{person.artwork.city}</h3>
+      <img src={person.artwork.image} alt={person.artwork.title} />
     </>
   );
 }
